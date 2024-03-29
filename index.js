2ea92c95b5006d5566b4eb131b494c238790b7c9
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-03-29 11:44:22 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector("main#main")
+    main.remove();
+
+let  newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "is the champion"
+
+
+
+
